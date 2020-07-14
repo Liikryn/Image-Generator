e17b0be579d15b57aded3fdd2159fddd8651cdcf
Liikryn <44905126+Liikryn@users.noreply.github.com>
2020-07-14 17:48:32 -0400

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import { Provider } from "react-redux"
+import { createStore, applyMiddleware } from "redux"
+import thunk from "redux-thunk"
+import { composeWithDevTools } from "redux-devtools-extension"
+
+import App from "./App"
+import rootReducer from "./reducers/index"
+import * as serviceWorker from "./serviceWorker"
+
+import "bootstrap/dist/css/bootstrap.css"
+
+
+ReactDOM.render(
+  <Provider store={ createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk))) }>
+    <Router>
+      <Route exact path="/" component={ () => <App /> } />
+    </Router>
+  </Provider>,
+
+  document.getElementById("root")
+)
+
+serviceWorker.unregister()
